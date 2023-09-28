35c21ba7a55b9f2f9e9019107e6380a8d3083964
gerardobeltran <contacto@gerardobeltran.mx>
2023-09-28 00:22:57 -0700

    --

---
+++ 00 Primeros pasos/02 Radio y perimetro de circulo/prog.cpp
@@ -0,0 +1,11 @@
+/* Mi primer programa escrito en C++
+ Fecha:12-Agosto-2023*/
+#include<iostream>
+using namespace std;
+int main()
+ {
+/* Escribe un mensaje */ 
+cout <<  "Hola mundo" << endl;
+
+    return 0;
+  }
