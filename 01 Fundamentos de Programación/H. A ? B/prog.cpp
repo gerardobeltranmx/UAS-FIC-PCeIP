23d48337262a7a4ea4b8f8d7f5f01233f7aebd6c
Gerardo Beltrán <contacto@gerardobeltran.mx>
2023-10-02 11:58:13 -0700

    --

---
+++ 01 Fundamentos de Programación/H. A ? B/prog.cpp
@@ -0,0 +1,29 @@
+/*
+  Proposito: 
+   Autor: Gerardo Beltrán
+   Fecha: 
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  
+  int A, B, C;
+ // cout << "Dame el valor de A: "; 
+  cin >> A;
+ // cout << "Dame el valor de B: "; 
+  cin >> B;
+
+  C = A + B;
+  cout << C << endl;
+  C = A - B;
+  cout << C << endl;
+  C = A * B;
+  cout << C << endl;
+  C = A / B;
+  cout << C << endl;
+  C = A % B;
+  cout << C << endl;
+  return 0; 
+}
