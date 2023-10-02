0bab6bfb95c17bf2d1e5914a6c7cab32de874c75
Gerardo Beltrán <contacto@gerardobeltran.mx>
2023-10-02 11:24:36 -0700

    --

--- 01 Fundamentos de Programación/G. Dólares/prog.cpp
+++ 01 Fundamentos de Programación/G. Dólares/prog.cpp
@@ -1,13 +1,23 @@
 /*
-  Proposito: 
+  Proposito: Calculo de dolares a pesos
    Autor: Gerardo Beltrán
-   Fecha: 
+   Fecha: 2- Oct-2023
 */
 #include <iostream>
 
 using namespace std;
 
 int main() {
-  
-   return 0; 
+   int numDolares, numPesos, tipoCambio;
+  // Entrada de datos 
+  cout << "Indicar numero de dolares: ";
+  cin >> numDolares;
+  cout << "Tipo de cambio: ";
+  cin >> tipoCambio;
+
+   // Calculo de pesos
+  numPesos = tipoCambio * numDolares;
+
+  cout << numPesos << endl;  
+  return 0; 
 }
