d18919cc99f0b3ea18b2f4ea814c5000e177d208
Gerardo Beltrán <contacto@gerardobeltran.mx>
2023-10-04 11:44:43 -0700

    --

---
+++ 01 Fundamentos de Programación/K. Area_Triangulo/prog.cpp
@@ -0,0 +1,26 @@
+/*
+  Proposito: Calculo del area de un triangulo
+   Autor: Gerardo Beltrán
+   Fecha: 4-oct-2023
+*/
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+  
+   double base, altura, area;
+
+   //cout << "Indicar la base: ";
+   cin >> base;
+
+   //cout << "Indicar la altura: ";
+   cin >> altura;
+
+   area = base * altura / 2;
+   
+   cout << fixed << setprecision(3) << area << endl;
+   
+   return 0; 
+}
