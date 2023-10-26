80f07797f833418d07fb2d0d8032cdd97ac060c5
Gerardo beltrán <contacto@gerardobeltran.mx>
2023-10-26 11:41:35 -0700

    --

---
+++ 02 Tma de decisiones/K. Estaciones/prog.cpp
@@ -0,0 +1,43 @@
+/*
+  Proposito: 
+   Autor: Gerardo Beltrán
+   Fecha: 
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+   int N;
+   cin >> N;
+   switch(N){
+      case 3:
+      case 4:
+      case 5:
+         cout << "PRIMAVERA" << endl;
+         break;
+      case 6:
+      case 7:
+      case 8:
+         cout << "VERANO" << endl;
+         break;
+      case 9:
+      case 10:
+      case 11:
+         cout << "OTONIO" << endl;
+         break;
+      case 12:
+      case 1:
+      case 2:
+         cout << "INVIERNO" << endl;
+         break;
+      default: 
+         cout << "ERROR" << endl;  
+         
+         
+   }
+   return 0; 
+
+
+}
