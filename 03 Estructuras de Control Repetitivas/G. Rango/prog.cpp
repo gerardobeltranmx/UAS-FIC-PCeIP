f9ae22dbf5709614a33a326a702dfa42eb256af8
Gerardo beltrán <contacto@gerardobeltran.mx>
2023-11-15 11:30:27 -0700

    --

---
+++ 03 Estructuras de Control Repetitivas/G. Rango/prog.cpp
@@ -0,0 +1,21 @@
+/*
+  Proposito: 
+   Autor: Gerardo Beltrán
+   Fecha: 
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n, m;
+
+  cin >> n; 
+  cin >> m;
+
+  for (int i=n; i <=m; i++ )
+      cout << i << endl;  
+
+
+   return 0; 
+}
