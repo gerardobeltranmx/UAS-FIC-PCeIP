13b644232b9e131ee6d921cd18c042823466060b
Gerardo beltrán <contacto@gerardobeltran.mx>
2023-12-01 12:05:34 -0700

    --

---
+++ 03 Estructuras de Control Repetitivas/Q. Calculando los promedios/prog.cpp
@@ -0,0 +1,32 @@
+/*
+  Proposito: 
+   Autor: Gerardo Beltrán
+   Fecha: 
+*/
+#include <iostream>
+#include <iomanip>
+
+
+using namespace std;
+
+int main() {
+   int k, mayor, menor, dato, suma=0;
+   cin >> k;
+
+   cin >> mayor;
+   menor = mayor;
+   suma=mayor;
+   for (int i=1; i < k; i++){
+         cin >> dato;
+         if (dato > mayor) mayor = dato;
+         if (dato < menor) menor = dato;
+         suma = suma + dato;
+   }
+   if (suma % k == 0)
+      cout << suma / k << endl;
+   else
+      cout << fixed << setprecision(3)<< (float)suma / k << endl; 
+
+
+   return 0; 
+}
